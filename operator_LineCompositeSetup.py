637962eb7a13db194eb5a0b232ccdb32e66164ad
ich <ichi84@gmail.com>
2020-12-13 20:01:05 +0900

    安定板

--- operator_LineCompositeSetup.py
+++ operator_LineCompositeSetup.py
@@ -4,8 +4,9 @@ import os
 #Library File info
 BaseDir = os.path.dirname(os.path.realpath(__file__)) + "\\blend\\"
 blendfile = "AOV.blend"
-AssetName = "Material"
-OverRideMaterialName  = "AOV"
+MaterialName  = "AOV"
+CompositeNodeName = "AOV.CompositNode"
+NodeStep = (300, 0)
 
 #Cycles Settings
 def SetCyclesSetting(scene):
@@ -17,18 +18,87 @@ def SetCyclesSetting(scene):
     for vl in scene.view_layers:
         vl.use_pass_uv = True
 
-
-#Get PreMeked Materials
+#Set PreMeked Materials
 def OverRideMaterials(scene):
     #get Material from .blend file
-    directory = BaseDir + blendfile + "\\" + AssetName + "\\"
-    bpy.ops.wm.link(
-        filename=OverRideMaterialName,
-        directory=directory)
-
+    directory = BaseDir + blendfile + "\\Material\\"
+    if not MaterialName in bpy.data.materials:
+        bpy.ops.wm.append(   #link
+            filename=MaterialName,
+            directory=directory)
     #OverrRide Material All Scenes
     for vl in scene.view_layers:
-        vl.material_override = bpy.data.materials[OverRideMaterialName]
+        vl.material_override = bpy.data.materials[MaterialName]
+
+def LinkNodeSameSockets(scene, fromNode, toNode):
+    for o in fromNode.outputs:
+        for i in toNode.inputs:
+            if o.name == i.name:
+                scene.node_tree.links.new(fromNode.outputs[o.name], toNode.inputs[i.name])
+
+def CreateFileSaveAllPasses():
+    output_path = context.preferences.filepaths.render_output_directory
+    node = scene.node_tree.nodes.new('CompositorNodeOutputFile')
+
+
+def AddCompositeNode(scene, context):
+    directory = BaseDir + blendfile + "\\NodeTree\\"
+    bpy.ops.wm.link( 
+        filename=CompositeNodeName,
+        directory=directory)
+
+    if not CompositeNodeName in scene.node_tree.nodes:
+        pos = [0,0]
+        node0 = scene.node_tree.nodes.new('CompositorNodeRLayers')
+        node0.location = pos
+
+        node1 = scene.node_tree.nodes.new('CompositorNodeGroup')
+        node1.node_tree =  bpy.data.node_groups[CompositeNodeName]
+        node1.name = CompositeNodeName
+        pos[0] += NodeStep[0]
+        pos[1] += NodeStep[1]
+        node1.location = pos
+
+        LinkNodeSameSockets(scene, node0, node1)
+
+        node2 = scene.node_tree.nodes.new('CompositorNodeMixRGB')
+        pos[0] += NodeStep[0]
+        pos[1] += NodeStep[1]
+        node2.location = pos
+        node2.blend_type = 'MULTIPLY'
+        scene.node_tree.links.new(node1.outputs['Value'], node2.inputs[1])
+
+        selectedNodes = [i for i in scene.node_tree.nodes if i.select]
+        
+        for l in scene.node_tree.links:
+            if l.to_node.type == 'COMPOSITE':
+                lastSocket = l.from_socket
+        scene.node_tree.links.new(lastSocket, node2.inputs[2])
+
+        node3 = None
+        for n in scene.node_tree.nodes:
+            if n.type == 'COMPOSITE':
+                node3 = n
+
+        if node3 == None:
+            node3 = scene.node_tree.nodes.new('CompositorNodeComposite')
+            node3.location = pos
+            pos[0] += NodeStep[0]
+            pos[1] += NodeStep[1]
+    
+        scene.node_tree.links.new(node2.outputs['Image'], node3.inputs['Image'])
+ 
+        node4 = scene.node_tree.nodes.new('CompositorNodeViewer')
+        pos[0] += NodeStep[0]
+        pos[1] += NodeStep[1]
+        node4.location = pos
+        scene.node_tree.links.new(node2.outputs['Image'], node4.inputs['Image'])
+        node3.select
+
+
+def addUVMap():
+    for o in [i for i in bpy.data.objects if i.type=='MESH']:
+         o.data.uv_layers.new(name="AOV.UV")
 
 def addAOV(scene, name):
     for vl in scene.view_layers:
@@ -37,7 +107,6 @@ def addAOV(scene, name):
                 return        
     bpy.ops.cycles.add_aov()
     vl.cycles.aovs[-1].name = name  
-
 
 def getAllNodes(NodeTree):
     result = []
@@ -58,7 +127,6 @@ def SetAOVfromMaterials(scene):
     for n in nodes:
         if(n.type == "OUTPUT_AOV"):
             addAOV(scene, n.name)
-
 
 def CreateAndLinkScene(context, NameExt):
     scene = context.scene
@@ -87,30 +155,29 @@ def CreateAndLinkScene(context, NameExt):
 
 def start_main(context):
     preScene = context.window.scene 
+    #Each Scene
     for s in bpy.data.scenes:
+        s.use_nodes = True
         context.window.scene = s
         newScene = CreateAndLinkScene(context, ".Link.Line")
+        #Only new scene
         if newScene != None:
             context.window.scene = newScene
             SetCyclesSetting(newScene)
             OverRideMaterials(newScene)
             SetAOVfromMaterials(newScene)
-            newScene.use_nodes = True
+        AddCompositeNode(s, context)
+    
+    #All Data
+    addUVMap()
+    #Restore active scene
     context.window.scene = preScene
+ 
     #render Image
     #bpy.ops.render.render()
     #render Animation
     #bpy.ops.render.render(animation=True)
     
-
-
-"""
-    lapNode = bpy.data.scenes[0].node_tree.nodes.new(type='CompositorNodeFilter')
-    lapNode.filter_type = 'LAPLACE'
-    mathNode = bpy.data.scenes[0].node_tree.nodes.new(type='CompositorNodeMath')
-    mathNode.operation = 'LESS_THAN'
-    mathNode.inputs['Value'].default_value = .001
-"""
 
 classes = []
 ####################################################
